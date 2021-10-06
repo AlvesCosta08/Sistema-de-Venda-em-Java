ce98f79f805b75f82405ec5348abca901381e195
AlvesCosta1974 <alvescosta74@gmail.com>
2021-10-06 19:52:18 -0300

    Criando metodos cadastrar,alterar e excluir

---
+++ src/br/com/projeto/dao/ClientesDAO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projeto.dao;
+
+/**
+ *
+ * @author AlvesDany
+ */
+public class ClientesDAO {
+    
+    // Metodo Caastrar clientes    
+    public void cadastrarClientes(){
+        
+    }
+    // Metodo alterarClientes
+    public void alterarClientes(){
+        
+    }
+    
+}
